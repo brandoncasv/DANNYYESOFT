a12d7a2e14c08f8c17578a6635290e81bca1054c
Brandon <brandoning777@gmail.com>
2020-10-13 16:47:00 -0500

    it was declared the CorporatesListcomponent

--- src/app/corporates/corporates.module.ts
+++ src/app/corporates/corporates.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CorporatesRoutingModule } from './corporates-routing.module';
 import { CorporatesListComponent } from './corporates-list/corporates-list.component';
 import { NgxDatatableModule } from "@swimlane/ngx-datatable";
+import { CorporateEditComponent } from './corporate-edit/corporate-edit.component';
 
 
 
 @NgModule({
-  declarations: [CorporatesListComponent],
+  declarations: [CorporatesListComponent, CorporateEditComponent],
   imports: [CommonModule, CorporatesRoutingModule, NgxDatatableModule],
 })
 export class CorporatesModule {}
