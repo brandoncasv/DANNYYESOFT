c885fa4045041ce2f8d948556441f966a829b19e
Brandon <brandoning777@gmail.com>
2020-10-14 16:10:21 -0500

    they were added libraries, NgbModule, NgxSpinnerModule, ReactiveFormsModule and FormsModule

--- src/app/corporates/corporates.module.ts
+++ src/app/corporates/corporates.module.ts
@@ -4,11 +4,20 @@ import { CorporatesRoutingModule } from './corporates-routing.module';
 import { CorporatesListComponent } from './corporates-list/corporates-list.component';
 import { NgxDatatableModule } from "@swimlane/ngx-datatable";
 import { CorporateEditComponent } from './corporate-edit/corporate-edit.component';
-
-
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [CorporatesListComponent, CorporateEditComponent],
-  imports: [CommonModule, CorporatesRoutingModule, NgxDatatableModule],
+  imports: [
+    CommonModule,
+    CorporatesRoutingModule,
+    NgxDatatableModule,
+    NgbModule,
+    NgxSpinnerModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
 export class CorporatesModule {}
